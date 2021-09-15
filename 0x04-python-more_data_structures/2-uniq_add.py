011fb68073a75dd3b591ce602c061d23cc9dc3eb
nadduli <naddulidaniel94@gmail.com>
2021-09-15 07:07:56 -0700

    created a unique list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = set(my_list)
-    result = 0
+    results = 0
     for i in new_list:
-        result += i
-        return result
+        results += i
+        return results
