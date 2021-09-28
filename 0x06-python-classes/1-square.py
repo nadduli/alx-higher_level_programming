a6327c51971908bd0a5978a0ebba0a7b01fb9c71
nadduli <naddulidaniel94@gmail.com>
2021-09-28 08:45:18 -0700

    created task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Represents an class of sqaure
+       Private instance attribute: size
+       Instantiation with size (no type/value verification)"""
+    def __init__(self, size):
+        """Initialize the data"""
+        self.__size = size
