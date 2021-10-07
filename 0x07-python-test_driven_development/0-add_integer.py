19eac5d4204adc12e0667d8eec54c3bac43dea1c
nadduli <naddulidaniel94@gmail.com>
2021-10-07 11:16:49 -0700

    modified task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""This module contains calculation functions"""
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
 
 
 def add_integer(a, b=98):
-    """"This function does the addition of 2 arguments
-    args:
-        a (union[int, float]): first number
-        b (union[int, float], optional): second number
-    returns:
-        the result of the addition
     """
-    if a is None or (type(a) is not int and type(a) is not float):
-        raise TypeError('a must be an integer')
-    if b is None or (type(b) is not int and type(b) is not float):
-        raise TypeError('b must be an integer')
-
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
-
-    return int(a) + int(b)
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
