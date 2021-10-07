fcd57510fc80995480547764ea036351e1093275
nadduli <naddulidaniel94@gmail.com>
2021-10-07 11:33:40 -0700

    fixed pep8 errors

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-'''this module do stuff to strings'''
+"""
+Module 5-text_indentation
+Contains method that prints text with 2 new lines after each ".", "?", and ":"
+Takes in a string
+"""
 
 
 def text_indentation(text):
-    '''this function fixes text indentation
-    args:
-        text (str): text to fix
-    returns:
-        None
-    '''
+    """
+    Prints text with 2 new lines after each ".", "?", and ":"
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-    if type(text) is not str:
-        raise TypeError('text must be a string')
-
-    fst_idx = 0
-    for idx, char in enumerate(text):
-        if char == '.' or char == '?' or char == ':':
-            if (idx + 1) == len(text):
-                tmp_str = text[fst_idx:]
-            else:
-                tmp_str = text[fst_idx:idx + 1]
-            fst_idx = idx + 1
-            print(tmp_str.strip())
-            print()
-            continue
-
-        if idx == len(text) - 1:
-            tmp_str = text[fst_idx:]
-            print(tmp_str.strip())
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    revised = "\n".join(list_lines)
+    print(revised, end="")
