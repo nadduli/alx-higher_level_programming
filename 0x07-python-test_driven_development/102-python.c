b7ff745d293d5673ec6be3e4c0ceaf045face515
nadduli <naddulidaniel94@gmail.com>
2021-10-07 12:23:29 -0700

    hello

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,29 +1,28 @@
-#include <Python.h>
-#include <object.h>
-#include <unicodeobject.h>
+#include <python.h>
 
-
+/**
+ * print_python_string - function that prints Python strings
+ * @p: Python string
+ * Return: void
+ */
 void print_python_string(PyObject *p)
 {
-    const char *type = NULL;
-    Py_ssize_t len = 0;
-    wchar_t *str = NULL;
+	ssize_t len;
 
-    printf("[.] string object info\n");
-    if (!PyUnicode_Check(p))
-    {
-        printf("  [ERROR] Invalid String Object\n");
-        return;
-    }
+	printf("[.] string object info\n");
 
-    if (PyUnicode_IS_COMPACT_ASCII(p))
-        type = "compact ascii";
-    else
-        type = "compact unicode object";
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
 
-    str = PyUnicode_AsWideCharString(p, &len);
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
 
-    printf("  type: %s\n", type);
-    printf("  length: %ld\n", len);
-    printf("  value: %ls\n", str);
+	len = PyUnicode_GET_LENGTH(p);
+	printf("  length: %ld\n", len);
+	printf("  value: %s\n", PyUnicode_AsUTF8AndSize(p, &len));
 }
---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,7 @@
+#ifndef PYTHON_H_
+#define PYTHON_H_
+
+void print_python_string(PyObject *p);
+
+
+#endif
