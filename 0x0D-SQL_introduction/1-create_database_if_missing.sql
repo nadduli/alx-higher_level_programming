c2abf167287d7b6f2bd0982029721ce13419f767
nadduli <naddulidaniel94@gmail.com>
2021-11-16 17:19:06 +0300

    create database if it doesnt existing databases

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- create a database if it doesnt exist mysql server
 -- create a database if it doesnt exist on mysql server
-CREATE DATABASE IF NOT EXIST hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
