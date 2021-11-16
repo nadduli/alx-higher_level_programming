cd46761fc5a2dc9cee0bea7c619d1f1f9613ffbe
nadduli <naddulidaniel94@gmail.com>
2021-11-16 21:54:57 +0300

    list all records in descending order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- list all reocrds of the table seocnd_table of the database hbtn_0c_0 in MYSQL server
 -- print all records
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
