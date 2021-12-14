941b1ffa8feb7d79fa1295bd93c41a5e240992a4
nadduli <naddulidaniel94@gmail.com>
2021-12-14 22:28:30 +0300

    classes and its properties

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,10 +3,11 @@
   defining a rectangle class with its properties
   height and width
 */
+
 class Rectangle {
     constructor (w, h) {
-	this.w = w;
-	this.h = h;
+	this.width = w;
+	this.height = h;
     }
 }
 
