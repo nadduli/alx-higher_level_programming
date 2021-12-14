d7580bc7c29dea168884774a043d656090adecbf
nadduli <naddulidaniel94@gmail.com>
2021-12-14 22:26:06 +0300

    classes and its properties

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+  defining a rectangle class with its properties
+  height and width
+*/
+class Rectangle {
+    constructor (w, h) {
+	this.w = w;
+	this.h = h;
+    }
+}
+
+module.exports = Rectangle;
