b8ba5e130f68b468cc81be4ee5a631efb28fce67
nadduli <naddulidaniel94@gmail.com>
2021-12-17 21:20:00 +0300

    select all states where name passed as an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" list all states from states table
+    where name is an argument
+    order by state id from hbtn_0e_0_usa """
+if __name__ == '__main__':
+    
+    import MYSQLdb
+    import sys
+    
+    db = MYSQLdb.connect(host="localhost", port=3306,
+                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
