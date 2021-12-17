dd159edaf040b95bb7468eb86a9bc6c610b38a6e
nadduli <naddulidaniel94@gmail.com>
2021-12-17 21:32:05 +0300

    select all states where name passed as an argument

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" list all states from state table
+    from hbtn_0e_4_usa where name 
+    is passed as an argument and
+    free from sql injection """
+if __name__ == '__main__':
+
+    import sys
+    import MYSQLdb
+
+    db = MYSQLdb.connect( host='localhost', port=3306,
+                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s 
+                 ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
