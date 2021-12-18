f25d4f038cd48b2bf62e885e2fd904666c56516f
nadduli <naddulidaniel94@gmail.com>
2021-12-19 00:23:33 +0300

    added task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-'''script  for task 7'''
+'''script for task 8'''
 
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 import sys
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     host = 'localhost'
     port = '3306'
+
     engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-             username, password, host, port, db_name), pool_pre_ping=True)
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True, poolclass=NullPool)
     Session = sessionmaker(bind=engine)
     local_session = Session()
-    states = local_session.query(State).order_by(State.id).all()
+    result = local_session.query(State).order_by(State.id).first()
     local_session.close()
-    engine.dispose()
 
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
+    if result:
+        print('{}: {}'.format(result.id, result.name))
+    else:
+        print('warning error')
