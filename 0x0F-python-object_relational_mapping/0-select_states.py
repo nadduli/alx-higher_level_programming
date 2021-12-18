ce0a0b3b707dfaf759ba1a8c986a8e5fa58a897e
nadduli <naddulidaniel94@gmail.com>
2021-12-18 18:26:58 +0000

    fixed some codes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ list all files in hbtn_0e_0_usa order by states.id """
 if __name__ == '__main__':
-    import MYSQLdb
+    import MySQLdb
     import sys
-    db = MYSQLdb.connect (host = "localhost", port = 3306, user = sys.argv[1], password = sys.argv[2], db = sys.argv[3])
+    db = MySQLdb.connect (host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM states ORDER_BY states.id ASC;")
+    mycursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     info = mycursor.fetchall()
     for items in info:
         print(items)
