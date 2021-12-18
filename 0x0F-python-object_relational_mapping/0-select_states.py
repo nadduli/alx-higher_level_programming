bd7544e4820d74dbcb99c9564c4df18583f95307
nadduli <naddulidaniel94@gmail.com>
2021-12-18 18:35:04 +0000

    fixed some codes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ list all files in hbtn_0e_0_usa order by states.id """
+
 if __name__ == '__main__':
+
     import MySQLdb
     import sys
     db = MySQLdb.connect (host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
