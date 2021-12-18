ac415349ee7af2a792ed1c5fb1ff8a5e931d80a0
nadduli <naddulidaniel94@gmail.com>
2021-12-19 00:18:22 +0300

    get all states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
+'''script  for task 7'''
+
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
+    db_name = sys.argv[3]
     host = 'localhost'
     port = '3306'
-    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(username, password, database_name, host, port, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+             username, password, host, port, db_name), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state_info = session.query(State).order_by(State.id).all()
-    session.close()
+    local_session = Session()
+    states = local_session.query(State).order_by(State.id).all()
+    local_session.close()
     engine.dispose()
-    
-    for state in state_info:
-        print('{}: {}'.format(State.id, State.name))
-    
+
+    for state in states:
+        print(str(state.id) + ': ' + state.name)
