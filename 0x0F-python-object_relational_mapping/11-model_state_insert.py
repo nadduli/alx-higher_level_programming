6a80e60c4309dc4d4e82b34dedddc07ae849dfb2
nadduli <naddulidaniel94@gmail.com>
2021-12-19 23:29:16 +0300

    added task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" add state data to table """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, database_name, host, port
+                           pool_pre_ping=True))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state1 = State('Louisiana')
+    session.add(state1)
+    session.commit
+
+    print(state1.id)
+    session.close()
+    engine.dispose()
