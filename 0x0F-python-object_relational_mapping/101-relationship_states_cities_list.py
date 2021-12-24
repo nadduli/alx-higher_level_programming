ff8a07cd70040458fedd5777e25e8740fac7425c
nadduli <naddulidaniel94@gmail.com>
2021-12-24 09:58:26 +0000

    fixed some checks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
+'''script for task 16'''
 
-if __name__ == "__main__":
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+import sys
 
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                            username, password, host, port, db_name
+                            ), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    loc_session = Session()
+    states = loc_session.query(State).order_by(State.id).all()
 
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-    session.close()
+            print('\t{}: {}'.format(city.id, city.name))
+
+    loc_session.close()
+    engine.dispose()
